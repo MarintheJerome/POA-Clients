51449d8e96833cb9c708e9ecf0153e1505c50f9d
MarintheJerome <jerome.marinthe@gmail.com>
2016-12-06 14:20:38 +0100

    vérif erreur page négociation

--- agent-client/src/main/java/castagnos/agent/client/controller/NegociationController.java
+++ agent-client/src/main/java/castagnos/agent/client/controller/NegociationController.java
@@ -1,11 +1,13 @@
 package castagnos.agent.client.controller;
 
 import castagnos.agent.client.agent.AgentClient;
+import castagnos.agent.client.common.Popup;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
-import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -13,7 +15,7 @@ import java.util.ResourceBundle;
 /**
  * Created by jerome on 06/12/2016.
  */
-public class NegociationController{
+public class NegociationController implements Initializable{
 
     @FXML
     private RadioButton radioSimple;
@@ -28,7 +30,7 @@ public class NegociationController{
     private RadioButton radioReponseNegative;
 
     @FXML
-    private ComboBox<?> listProduits;
+    private ComboBox<String> listProduits;
 
     @FXML
     private TextField quantiteNegociation;
@@ -41,6 +43,26 @@ public class NegociationController{
 
     private AgentClient agentClient;
 
+    public void initialize(URL location, ResourceBundle resources) {
+        // Remplissage combobox produit. A virer une fois que tout sera bien récupéré
+        listProduits.getItems().add("Sauce tomate");
+        listProduits.getItems().add("Purée");
+        listProduits.getItems().add("Sandwich trop bon");
+        listProduits.getItems().add("Quenelle");
+        listProduits.getItems().add("Kebab");
+
+        // On force le textfield pour la quantité à contenir que des nombres
+        quantiteNegociation.textProperty().addListener(new ChangeListener<String>() {
+
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    quantiteNegociation.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+    }
+
     public void loadRessource(AgentClient ac){
         agentClient = ac;
         ArrayList<String> listAgents = agentClient.getOthers("Client");
@@ -51,6 +73,24 @@ public class NegociationController{
 
     @FXML
     public void demander() {
+        if(!checkErrors()){
 
+        }
+    }
+
+    private boolean checkErrors() {
+        if(listeClient.getSelectionModel().isEmpty()){
+            Popup.popUpError("Erreur", "Veuillez choisir un client");
+            return true;
+        }
+        if(listProduits.getSelectionModel().isEmpty()){
+            Popup.popUpError("Erreur", "Veuillez choisir un produit");
+            return true;
+        }
+        if(quantiteNegociation.getText().equals("")){
+            Popup.popUpError("Erreur", "Veuillez rentrer une quantité");
+            return true;
+        }
+        return false;
     }
 }
